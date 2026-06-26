52213938f22623c64b5b17264b63933f19b37038
NgurahDhananjaya <99886528+NgurahDhananjaya@users.noreply.github.com>
2026-06-26 16:07:28 +0800

    feat: Laptop and Peminjaman Migration, Models and Status Enums

---
+++ database/migrations/2026_06_26_074235_create_laptop_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Enums\LaptopStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laptop', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('kode')->unique();
+            $table->string('nama');
+            $table->text('spesifikasi')->nullable();
+            $table->string('status')->default(LaptopStatus::TERSEDIA->value);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laptop');
+    }
+};
